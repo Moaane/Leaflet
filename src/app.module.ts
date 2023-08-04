b46bf41e0bc6cd078f70cc8b5feb556a2bda8317
Moaane <gpro0150@gmail.com>
2023-08-04 11:21:04 +0700

    au ah

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypePlaceModule } from './type-place/type-place.module';
 import { PlaceModule } from './place/place.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PlacetypeModule } from './placetype/placetype.module';
-import { TypeplaceController } from './typeplace/typeplace.controller';
-import { TypePlaceModule } from './type-place/type-place.module';
 
 @Module({
-  imports: [TypePlaceModule, PlacetypeModule, PrismaModule, PlaceModule],
-  controllers: [AppController, TypeplaceController],
+  imports: [PlacetypeModule, PrismaModule, PlaceModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
