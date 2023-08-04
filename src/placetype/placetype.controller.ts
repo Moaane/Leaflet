2b9033001b3b8a4a65de639362df5279dfc8a14f
Moaane <gpro0150@gmail.com>
2023-08-04 10:42:31 +0700

    .

--- src/placetype/placetype.controller.ts
+++ src/placetype/placetype.controller.ts
@@ -1,4 +1,6 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('placetype')
-export class PlacetypeController {}
+export class PlacetypeController {
+    
+}
--- src/placetype/placetype.module.ts
+++ src/placetype/placetype.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlacetypeController } from './placetype.controller';
 import { PlacetypeService } from './placetype.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
-  controllers: [PlacetypeController, ],
-  providers: [PlacetypeService]
+  providers: [PlacetypeService, PrismaService],
+  controllers: [PlacetypeController]
 })
 export class PlacetypeModule {}
