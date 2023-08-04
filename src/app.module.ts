35b602a33def87bce46eb449270f9344fba6fc60
FathirTHX <fathirisgaming12@gmail.com>
2023-08-04 10:43:17 +0700

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypePlaceModule } from './type-place/type-place.module';
+import { PlaceModule } from './place/place.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PlacetypeModule } from './placetype/placetype.module';
 import { TypeplaceController } from './typeplace/typeplace.controller';
 import { TypePlaceModule } from './type-place/type-place.module';
 
 @Module({
-  imports: [TypePlaceModule, PlacetypeModule, PrismaModule],
+  imports: [TypePlaceModule, PlacetypeModule, PrismaModule, PlaceModule],
   controllers: [AppController, TypeplaceController],
   providers: [AppService],
 })
