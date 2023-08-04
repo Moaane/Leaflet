791bdaffbc0c4998843b70a715db061b40b223d1
FathirTHX <fathirisgaming12@gmail.com>
2023-08-04 14:36:33 +0700

    konotl

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3212);
-}
+  await app.listen(process.env.PORT,'192.168.18.210',()=>{
+    console.log('KONTOL')
+  }
+)}
 bootstrap();
