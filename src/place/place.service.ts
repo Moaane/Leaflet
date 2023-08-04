3a037c51f384c1bd332077e883d3b2a1d84c3379
FathirTHX <fathirisgaming12@gmail.com>
2023-08-04 10:58:35 +0700

    siu

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { place } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class PlaceService {}
+export class PlaceService {
+    constructor (private prismaService : PrismaService){}
+
+    async findAll(): Promise<place[]>{
+        return this.prismaService.place.findMany()
+    }
+
+    async findById(id : string): Promise<place>{
+        return this.prismaService.place.findUnique({where:{id}})
+    }   
+
+    async createData(data : {name : string, description : string , latitude : string , longitude : string, type_placeid:string}): Promise<place>{
+        return this.prismaService.place.create({data})
+    }
+
+    async updataData(id : string, data : {name : string, description : string , latitude : string , longitude : string, type_placeid:string}): Promise<place>{
+        return this.prismaService.place.update({
+            where:{
+                id
+            },
+            data,
+        });
+    }
+
+    }
